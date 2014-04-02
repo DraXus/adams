4d0f4a7849091330456720386a82ef4d2da5bffa
Manuel Martín <draxus@gmail.com>
2014-04-02 18:33:11 +0100

    Listing classes

--- adams-manuel/src/adams/flow/template/MyTransformer.java
+++ adams-manuel/src/adams/flow/template/MyTransformer.java
@@ -22,11 +22,15 @@ package adams.flow.template;
 import java.util.Arrays;
 
 import weka.core.Instances;
+import adams.core.ClassLister;
+import adams.core.base.BaseRegExp;
 import adams.core.base.BaseString;
 import adams.core.io.PlaceholderFile;
+import adams.core.option.OptionUtils;
 import adams.data.conversion.SpreadSheetToWekaInstances;
 import adams.data.io.input.CsvSpreadSheetReader;
 import adams.data.spreadsheet.SpreadSheet;
+import adams.env.Environment;
 import adams.flow.control.Flow;
 import adams.flow.core.AbstractActor;
 import adams.flow.sink.Display;
@@ -39,7 +43,7 @@ public class MyTransformer extends AbstractActorTemplate {
 
 	/** the file to load. */
 	protected PlaceholderFile m_TrainingFile;
-	
+
 	/** options for the csv reader */
 	protected CsvSpreadSheetReader m_CsvReader;
 
@@ -64,7 +68,7 @@ public class MyTransformer extends AbstractActorTemplate {
 		m_OptionManager.add("csvReader", "csvReader",
 				new CsvSpreadSheetReader());
 	}
-	
+
 	/**
 	 * Sets the training file to load.
 	 * 
@@ -93,7 +97,7 @@ public class MyTransformer extends AbstractActorTemplate {
 	public String trainingFileTipText() {
 		return "The training file to load.";
 	}
-	
+
 	/**
 	 * Sets the csv reader.
 	 * 
@@ -149,80 +153,126 @@ public class MyTransformer extends AbstractActorTemplate {
 	@Override
 	protected AbstractActor doGenerate() {
 		// 1. Read file
-		//TODO Extend to other types of files
-	    SpreadSheet sheet = m_CsvReader.read(m_TrainingFile);
-	    
-	    // Converting to Weka format to use Weka methods
-	    SpreadSheetToWekaInstances m_Conversion = new SpreadSheetToWekaInstances();
-	    m_Conversion.setInput(sheet);
-	    String out = m_Conversion.convert();
-	    if(out!=null)
-	    	return null; //TODO throw conversion error
-	    
-	    Instances m_Data = (Instances) m_Conversion.getOutput();
-	    m_Conversion.cleanUp();
-	    System.out.println("Weka conversion complete");
-	    
-	    
-	    
-		
-	    // 2. Extract data characteristics
-	    int numRows = sheet.getRowCount();
-	    int numColumns = sheet.getColumnCount();
-	    int[] numMissingValuesByRow = getMissingValuesByRow(m_Data);
-	    int[] numMissingValuesByColumn = getMissingValuesByColumn(m_Data);
-	    
-	    StringConstants characteristics = new StringConstants();
-	    characteristics.setStrings(new BaseString[]{
-	    		new BaseString("rows = "+numRows),
-	    		new BaseString("columns = "+numColumns),
-	    		new BaseString("missingValuesByRow = "+Arrays.toString(numMissingValuesByRow)),
-	    		new BaseString("missingValuesByColumn = "+Arrays.toString(numMissingValuesByColumn))
-	    	    });
-	    Display report = new Display();
-	    
-	    
-	    
+		// TODO Extend to other types of files
+		SpreadSheet sheet = m_CsvReader.read(m_TrainingFile);
+
+		// Converting to Weka format to use Weka methods
+		SpreadSheetToWekaInstances m_Conversion = new SpreadSheetToWekaInstances();
+		m_Conversion.setInput(sheet);
+		String out = m_Conversion.convert();
+		if (out != null)
+			return null; // TODO throw conversion error
+
+		Instances m_Data = (Instances) m_Conversion.getOutput();
+		m_Conversion.cleanUp();
+		System.out.println("Weka conversion complete");
+
+		// 2. Extract data characteristics
+		int numRows = sheet.getRowCount();
+		int numColumns = sheet.getColumnCount();
+		int[] numMissingValuesByRow = getMissingValuesByRow(m_Data);
+		int[] numMissingValuesByColumn = getMissingValuesByColumn(m_Data);
+
+		StringConstants characteristics = new StringConstants();
+		characteristics.setStrings(new BaseString[] {
+				new BaseString("rows = " + numRows),
+				new BaseString("columns = " + numColumns),
+				new BaseString("missingValuesByRow = "
+						+ Arrays.toString(numMissingValuesByRow)),
+				new BaseString("missingValuesByColumn = "
+						+ Arrays.toString(numMissingValuesByColumn)) });
+		Display report = new Display();
+
+		listClasses();
+
 		// 3. Generate workflow based on the data characteristics
-	    Flow flow = new Flow();
-	    flow.setActors(new AbstractActor[]{characteristics, report});
+		Flow flow = new Flow();
+		flow.setActors(new AbstractActor[] { characteristics, report });
 		return flow;
 	}
-	
+
 	/**
 	 * Counts the number of missing values in each row
 	 * 
 	 * @return array with the number of missing values in each row
 	 */
-	private int[] getMissingValuesByRow(Instances data){
+	private int[] getMissingValuesByRow(Instances data) {
 		int numRows = data.size();
 		int numColumns = data.numAttributes();
-		
+
 		int[] missingValues = new int[numRows];
-		for(int i=0; i<numRows; i++)
-			for(int j=0; j<numColumns; j++)
-				if(data.get(i).isMissing(j))
+		for (int i = 0; i < numRows; i++)
+			for (int j = 0; j < numColumns; j++)
+				if (data.get(i).isMissing(j))
 					missingValues[i]++;
-			
+
 		return missingValues;
 	}
-	
+
 	/**
 	 * Counts the number of missing values in each column
 	 * 
 	 * @return array with the number of missing values in each column
 	 */
-	private int[] getMissingValuesByColumn(Instances data){
+	private int[] getMissingValuesByColumn(Instances data) {
 		int numRows = data.size();
 		int numColumns = data.numAttributes();
-		
+
 		int[] missingValues = new int[numColumns];
-		for(int i=0; i<numRows; i++)
-			for(int j=0; j<numColumns; j++)
-				if(data.get(i).isMissing(j))
+		for (int i = 0; i < numRows; i++)
+			for (int j = 0; j < numColumns; j++)
+				if (data.get(i).isMissing(j))
 					missingValues[j]++;
-			
+
 		return missingValues;
+	}
+
+	private void listClasses() {
+		try {
+			ClassLister m_ClassLister = ClassLister.getSingleton();
+
+			// environment
+			String env = null;
+			if (env == null)
+				env = Environment.class.getName();
+			Class cls = Class.forName(env);
+			Environment.setEnvironmentClass(cls);
+
+			// match
+			String match = ".*\\.transformer\\..*";
+			if (match == null)
+				match = BaseRegExp.MATCH_ALL;
+			BaseRegExp regexp = new BaseRegExp(match);
+
+			// allow empty class hierarchies?
+			boolean allowEmpty = false;
+
+			// superclass
+			String[] superclasses;
+			String sclass = "adams.flow.core.AbstractActor";
+			if (sclass == null)
+				superclasses = m_ClassLister.getSuperclasses();
+			else
+				superclasses = new String[] { sclass };
+
+			// list them
+			for (String superclass : superclasses) {
+				cls = Class.forName(superclass);
+				String[] classnames = m_ClassLister.getClassnames(cls);
+				if ((classnames.length > 0) || allowEmpty) {
+					System.out.println("--> " + superclass);
+					for (String classname : classnames) {
+						if (regexp.isMatch(classname))
+							System.out.println(classname);
+					}
+					System.out.println();
+				}
+			}
+			System.out.println("ClassLister ends");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
